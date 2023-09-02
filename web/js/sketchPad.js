000d57ce73049b2eaf33bc7c6d560620d7652d77
arvindjangre <arvindbjangre@gmail.com>
2023-09-03 00:15:51 +0530

    get mouse coordinates

--- web/js/sketchPad.js
+++ web/js/sketchPad.js
@@ -1,5 +1,6 @@
 class SketchPad {
   constructor(container, size = 400) {
+    // make a canvas where we can draw
     this.canvas = document.createElement("canvas");
     this.canvas.width = size;
     this.canvas.height = size;
@@ -8,5 +9,23 @@ class SketchPad {
       box-shadow: 0px 0px 10px 2px black;
     `;
     container.appendChild(this.canvas);
+
+    // to draw using mouse
+    this.ctx = this.canvas.getContext('2d');
+    // # means private method
+    this.#addEventListeners();
+
+  }
+
+  #addEventListeners() {
+    this.canvas.onmousedown = (evt) => {
+      const rect = this.canvas.getBoundingClientRect();
+      // coordinates x,y
+      const mouse = [
+        evt.clientX - rect.left,
+        evt.clientY - rect.top
+      ];
+      console.log(mouse);
+    }
   }
 }
