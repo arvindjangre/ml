569e07bb7264496808c9d74ef9a0b3e0510031bb
arvindjangre <arvindbjangre@gmail.com>
2023-09-03 03:58:03 +0530

    generated image dataset from raw json

---
+++ common/utils.js
@@ -0,0 +1,18 @@
+// progress bar
+const utils = {};
+utils.formatPercent = () => {
+  return (n * 100).toFixed(2) + '%';
+}
+
+utils.printProgress = (cont, max) => {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  const percent = utils.formatPercent(
+    count/max
+  );
+  process.stdout.write(count+'/'+max+ ' (' + percent + ')');
+}
+
+if(typeof module !== 'undefined') {
+  module.exports = utils; 
+}
