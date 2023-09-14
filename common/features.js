d34d87e916126602fcd7dfc24cd82d256b443bb8
arvindjangre <arvindbjangre@gmail.com>
2023-09-14 16:49:24 +0530

    feature extraction

---
+++ common/features.js
@@ -0,0 +1,16 @@
+
+// extract features
+const features = {};
+
+features.getPathCount = (paths) => {
+  return paths.length;
+}
+
+features.getPointCount = (paths) => {
+  const points = paths.flat();
+  return points.length;
+}
+
+if(typeof module !== 'undefined') {
+  module.exports = features;
+}
